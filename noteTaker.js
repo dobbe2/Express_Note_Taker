e7fc625df8984980ceff3025a8e0c9d1ce32bd3d
James Dobbe <dobbe2@gmail.com>
2020-02-27 21:01:22 -0800

    .gitignore fix

--- noteTaker.js
+++ noteTaker.js
@@ -3,36 +3,44 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const http = require("http");
-//Set up ExPress
+
+//Set up Express server
 const PORT = 8080;
 const app = express();
-const server = http.createServer(handleRequest);
+// const server = http.createServer(handleRequest);
 
+app.use(express)
+
+//HTML requests
+
+app.get("/develop/public/notes.html", function (req, res){
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+})
 
 //Start the server and begin listening
-server.listen(PORT, function(){
+app.listen(PORT, function(){
     console.log("Server listening on: http://localhost:" + PORT);
 });
 
 
-function handleRequest(req, res){
-    let path = req.url;
+// function handleRequest(req, res){
+//     let path = req.url;
 
-    switch (path) {
+//     switch (path) {
 
-        case"/*":
-            return fs.readFile(__dirname + "/develop/public/index.html", function(err, data) {
-                if (err) throw err;
-                res.writeHead(200, {"Content-Type": "text/html"});
-                res.end(data);
-            });
+//         case"/*":
+//             return fs.readFile(__dirname + "/develop/public/index.html", function(err, data) {
+//                 if (err) throw err;
+//                 res.writeHead(200, {"Content-Type": "text/html"});
+//                 res.end(data);
+//             });
 
-        case"/notes":
-            return fs.readFile(__dirname + "/develop/public/notes.html", function(err, data){
-                if (err) throw err;
-                res.writeHead(200, {"Content-Type": "text/html"});
-                res.end(data)
-            })
+//         case"/notes":
+//             return fs.readFile(__dirname + "/develop/public/notes.html", function(err, data){
+//                 if (err) throw err;
+//                 res.writeHead(200, {"Content-Type": "text/html"});
+//                 res.end(data)
+//             })
     
-    };
-}   
+//     };
+// }   
