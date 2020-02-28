982282112c0aa1875aa7bcd347a38bed7d4cbd41
James Dobbe <dobbe2@gmail.com>
2020-02-27 23:21:34 -0800

    trying to get the JSON data to pull up when visiting localhost:8080/api/notes , not haing luck. Created new DB file, same info as db.json, but made it a db.js so I could export it and require it for my api calls

--- noteTaker.js
+++ noteTaker.js
@@ -1,46 +1,43 @@
-//dependencies
+//required npms and dependencies
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
-const http = require("http");
-
+// const http = require("http");
+const notes = require("./Develop/db/db.js")
 //Set up Express server
 const PORT = 8080;
 const app = express();
-// const server = http.createServer(handleRequest);
 
-app.use(express)
+//create the notes data
+
+// let notes= [];
 
 //HTML requests
-
-app.get("/develop/public/notes.html", function (req, res){
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+app.get('/notes', function (req,res){
+    res.sendFile(path.join(__dirname, "Develop/public/notes.html"));
+    console.log("html get note")
 })
+
+app.get("*", function (req, res){
+    res.sendFile(path.join(__dirname, "Develop/public/index.html"))
+})
+
+
+//API routes
+//  /api/notes GET- read JSON file and return all saved JSON notes
+app.get("/api/notes", (req,res) => {
+    res.json(notes)
+    console.log(notes)
+    notes = JSON.parse(notes);
+});
+
+// //  /api/notes POST- recieve new note and save onto the request body, add to db.json, then return new note to client
+// app.post("/api/notes", function (req,res){
+//     notes.push(req.body);
+// })
+//  /api/notes/:id DELETE
 
 //Start the server and begin listening
 app.listen(PORT, function(){
     console.log("Server listening on: http://localhost:" + PORT);
 });
-
-
-// function handleRequest(req, res){
-//     let path = req.url;
-
-//     switch (path) {
-
-//         case"/*":
-//             return fs.readFile(__dirname + "/develop/public/index.html", function(err, data) {
-//                 if (err) throw err;
-//                 res.writeHead(200, {"Content-Type": "text/html"});
-//                 res.end(data);
-//             });
-
-//         case"/notes":
-//             return fs.readFile(__dirname + "/develop/public/notes.html", function(err, data){
-//                 if (err) throw err;
-//                 res.writeHead(200, {"Content-Type": "text/html"});
-//                 res.end(data)
-//             })
-    
-//     };
-// }   
